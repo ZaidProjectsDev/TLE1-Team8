3af07655db8656a2049847de2c721d9170776d15
Ryan de Man <Ryan.m.de.man@gmail.com>
2023-11-08 13:31:50 +0100

    Python starting template

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2 as cv
 import numpy as np
 import os 
@@ -64,3 +65,30 @@ def findClickPositions(needle_img_path,reference_img_path, threshold = 0.5, debu
 
 points = findClickPositions(needle_img_path_real,canvas_img_path_real,0.8,debug_mode='rectangles');
 print(points)
+=======
+import tkinter as tk
+from screen_reader import toggle_screen_reader
+
+root = tk.Tk()
+root.title("Screen Reader App")
+
+# Styling
+root.geometry("400x300")  # Set the window size
+root.configure(bg="white")  # Set the background color
+
+canvas1 = tk.Canvas(root, width=400, height=300, bg="white")  # Set canvas background color
+canvas1.pack()
+
+button1 = tk.Button(
+    text="Enable Screen Reader",
+    command=lambda: toggle_screen_reader(button1, canvas1, root),
+    bg="green",
+    fg="white",
+    font=("Arial", 14),
+    padx=20,
+    pady=10,
+)
+canvas1.create_window(200, 150, window=button1)
+
+root.mainloop()
+>>>>>>> 97fb353 (Python starting template)
