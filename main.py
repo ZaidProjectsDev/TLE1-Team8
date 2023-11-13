a4db1953832e51340bbe775d7abaa501251d5db2
ZaidDev <44425998+ZaidProjectsDev@users.noreply.github.com>
2023-11-13 15:06:46 +0100

    Merged main

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 canvas_img_path_real = 'img-test/ref/tew2/tew2_ref_2.png'
 needle_img_path_real = 'img-test/ref/tew2/goal/gui_2.png'
 game_window_name = 'The Evil Within 2' #Replace this with what is relevant to your use case.
-while (True):
-    keyboardInput(game_window_name)
-    if(cv.waitKey(1)==ord('p')):
-        cv.destroyAllWindows()
-        break
+
 
 #points = findClickPositions(needle_img_path_real,canvas_img_path_real,0.8,debug_mode='rectangles');
 #print(points)
@@ -39,5 +35,9 @@ button1 = tk.Button(
     pady=10,
 )
 canvas1.create_window(200, 150, window=button1)
-
+while (True):
+    keyboardInput(game_window_name)
+    if(cv.waitKey(1)==ord('p')):
+        cv.destroyAllWindows()
+        break
 root.mainloop()
