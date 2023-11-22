39b7f9db991a1b6631d8f0af2740749fdf9734dd
ZaidDev <44425998+ZaidProjectsDev@users.noreply.github.com>
2023-11-22 15:04:46 +0100

    Revert "Feature/local variable"

--- inputchecker.py
+++ inputchecker.py
@@ -1,12 +1,9 @@
 import keyboard
 import pygame
-import speaker
+import pyttsx3
 import gamecapture
-import localization
 root = None
-models = None
-state_reporter = speaker.SpeakerObject()
-lang = localization.Localization()
+
 def play_sound(file_path):
     pygame.init()
     pygame.mixer.init()
@@ -15,28 +12,24 @@ def play_sound(file_path):
         sound = pygame.mixer.Sound(file_path)
         sound.play()
         pygame.time.delay(int(sound.get_length() * 1000))  # Delay to ensure the sound plays completely
-        state_reporter.say(lang.get_translation('tts_scanning'))
     except pygame.error as e:
         print("Error playing sound:", e)
     finally:
         pygame.mixer.quit()
 
 
-def scanner(game_window, shared_models):
+def scanner(game_window):
     print("you pressed 'q' .")
     play_sound('ding-36029.wav')
-    result = gamecapture.try_to_capture_game_window(20, game_window, shared_models.genericModel)
-    state_reporter.say(result)
+    result = gamecapture.try_to_capture_game_window(20,game_window)
+    engine = pyttsx3.init()
+    engine.say(result)
+    engine.runAndWait()
     print(result)
 
 
-def keyboardInput(game_window, shared_models):
+def keyboardInput(game_window):
     if keyboard.is_pressed('q'):
-        if game_window != '':
-            if shared_models.genericModel is None:
-                state_reporter.say(lang.get_translation('tts_not_initialized'))
-            else:
-                scanner(game_window, shared_models)
-        else:
-            state_reporter.say(lang.get_translation('tts_no_window_found'))
+        scanner(game_window)
+
 
--- main.py
+++ main.py
@@ -1,85 +1,49 @@
 import cv2 as cv
 import numpy as np
-import os
-
-import gamecapture
-import speaker
-import windowdefinition
+import os 
 from gamecapture import scan_game_window
 from inputchecker import keyboardInput
-import gui_windowcontroller
-import shared_model as current_vars
-
-shared_models = current_vars.SharedModels()
-current_vars.setCurrentSharedModel(shared_models)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import localization
+canvas_img_path_real = 'img-test/ref/tew2/tew2_ref_2.png'
+needle_img_path_real = 'img-test/ref/tew2/goal/gui_2.png'
+game_window_name = 'Ratchet & Clank: Rift Apart v1.922.0.0' #Replace this with what is relevant to your use case.
 
-lang = localization.Localization()
+
+#points = findClickPositions(needle_img_path_real,canvas_img_path_real,0.8,debug_mode='rectangles');
+#print(points)
+
 import tkinter as tk
 from screen_reader import toggle_screen_reader
-error_reporter = speaker.SpeakerObject()
-message_reporter = speaker.SpeakerObject()
 
 root = tk.Tk()
+root.title("Screen Reader App")
 
-# buttons
-btn_language = tk.Button(
-    text=lang.get_translation('language_toggle_button'),
-    command=lambda: lang.set_language(lang.language),
+# Styling
+root.geometry("400x300")  # Set the window size
+root.configure(bg="white")  # Set the background color
+
+canvas1 = tk.Canvas(root, width=400, height=300, bg="white")  # Set canvas background color
+canvas1.pack()
+
+button1 = tk.Button(
+    text="Enable Screen Reader",
+    command=lambda: toggle_screen_reader(button1, canvas1, root),
     bg="green",
     fg="white",
     font=("Arial", 14),
     padx=20,
     pady=10,
 )
-btn_language.pack()
-message_reporter.say(lang.get_translation('tts_app_started'))
+def keyboard_update():
+    keyboardInput(game_window_name)
+    root.after(10,keyboard_update)
 
-# # Styling
-# root.geometry("400x300")  # Set the window size
-# root.configure(bg="white")  # Set the background color
-#
-# canvas1 = tk.Canvas(root, width=400, height=300, bg="white")  # Set canvas background color
-# canvas1.pack()
-#
-# button1 = tk.Button(
-#     text="Enable Screen Reader",
-#     command=lambda: toggle_screen_reader(button1, canvas1, root),
-#     bg="green",
-#     fg="white",
-#     font=("Arial", 14),
-#     padx=20,
-#     pady=10,
-# )
-# def keyboard_update():
-#     keyboardInput(game_window_name)
-#     root.after(10,keyboard_update)
-#
-# canvas1.create_window(200, 150, window=button1)
+canvas1.create_window(200, 150, window=button1)
 
 # while (True):
 #     keyboardInput(game_window_name)
 #     if(cv.waitKey(1)==ord('p')):
 #         cv.destroyAllWindows()
 #         break
-
-def update_program():
-    keyboard_update()
-    gui_windowcontroller.shared_models = shared_models
-    gamecapture.shared_models = shared_models
-    gui_windowcontroller.update()
-    root.after(5, update_program)
-
-
-def keyboard_update():
-    # if windowdefinition.activeWindow is not None and windowdefinition.activeWindow.title is not '':
-    keyboardInput(windowdefinition.activeWindow.title, shared_models)
-
-    root.after(10, keyboard_update)
-
-
-
-
-update_program()
+keyboard_update()
 root.mainloop()
